0571bf31dd19bf9ed500d9da55781ecc5ef50550
Victor Ginzburg <v-viktor456@mail.ru>
2025-05-10 11:56:08 +0300

    first iteration

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +14,88 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size == 0 {
+		return []int{}
+	}
+
+	res := make([]int, size)
+
+	for i := range size {
+		res[i] = rand.Intn(SIZE) + 1 // +1, чтобы избежать нуля, который я возьму за ошибку
+	}
+
+	return res
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	mx := data[0]
+
+	for _, v := range data {
+		mx = max(mx, v)
+	}
+
+	return mx
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	if len(data) < 8 {
+		return maximum(data)
+	}
+
+	maxes := make([]int, CHUNKS)
+	chunkSize := len(data) / CHUNKS
+
+	wg := sync.WaitGroup{}
+	wg.Add(CHUNKS)
+
+	for i := range CHUNKS {
+		go func(i int) {
+			defer wg.Done()
+
+			start := i * chunkSize
+			end := start + chunkSize
+
+			if i == CHUNKS-1 {
+				end = len(data)
+			}
+
+			maxes[i] = maximum(data[start:end])
+		}(i)
+	}
+
+	wg.Wait()
+
+	return maximum(maxes)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+
+	nums := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+
+	timeStart := time.Now()
+	max := maximum(nums)
+	elapsed := time.Since(timeStart).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+
+	timeStart = time.Now()
+	max = maxChunks(nums)
+	elapsed = time.Since(timeStart).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,39 @@
 package main
 
 // Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+	result := generateRandomElements(0)
+	assert.Equal(t, []int{}, result)
+
+	result = generateRandomElements(10)
+	assert.Len(t, result, 10)
+
+	for _, v := range result {
+		assert.GreaterOrEqual(t, v, 0)
+		assert.LessOrEqual(t, v, SIZE)
+	}
+}
+
+func TestMaximum(t *testing.T) {
+	testCases := []struct {
+		data     []int
+		expected int
+	}{
+		{data: []int{}, expected: 0},
+		{data: []int{1}, expected: 1},
+		{data: []int{2, 2, 2, 2}, expected: 2},
+		{data: []int{1, 2, 3, 4, 5, 6}, expected: 6},
+		{data: []int{6, 5, 4, 3, 2, 1}, expected: 6},
+	}
+
+	for _, tc := range testCases {
+		result := maximum(tc.data)
+		assert.Equal(t, tc.expected, result)
+	}
+}
